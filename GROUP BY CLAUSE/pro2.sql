90da4da66f8a617220d200b57ce74f96c93c126c
Prashant Mehta <prashantmehta1207@gmail.com>
2026-05-01 17:19:44 +0530

    Create pro2.sql

---
+++ GROUP BY CLAUSE/pro2.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE COLLASS;
+USE COLLASS;
+
+CREATE TABLE STUDENSS(
+ROLLNO INT PRIMARY KEY,
+NAME VARCHAR(50) , 
+marks INT not null,
+grade VARCHAR(1),
+city VARCHAR(20)
+);
+
+INSERT INTO STUDENSS (rollno , NAME , marks , grade , city) value
+(100 , "Prashant" , 89 , "B" , "GOVA"),
+(101 , "JAY" , 45 , "A" , "DELHI"),
+(102 , "AMAN" , 78 , "C" , "MUMBAI"),
+(103 , "NEHA" , 87 , "A" , "ABA"),
+(104 , "MEHTA" , 17 , "F" , "MUMBAI"),
+(105 , "MONY" , 99 , "A" , "GOVA");
+
+SELECT DISTINCT city FROM STUDENSS;
+
+SELECT city , name , COUNT(rollno)
+FROM STUDENSS
+GROUP BY city , name ;
+
+
+
